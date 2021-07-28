fb89b1f57adc9433123c6a5c2acab7a071bf4564
Prasad Biswas <alterego14@pop-os.localdomain>
2021-07-28 15:39:14 +0100

    media try 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from src import app_factory
 
 app = app_factory()
-app.run(debug = True)
+app.run(debug = True , host="0.0.0.0")
--- src/static/js/today.js
+++ src/static/js/today.js
@@ -2,6 +2,7 @@ window.onload = function(){
 
     today = new Date().toLocaleDateString().split('/');
     document.getElementById('day').value = today[2] + "-" + today[1] + "-" + today[0];
+    alert("today")
 
 }
 
